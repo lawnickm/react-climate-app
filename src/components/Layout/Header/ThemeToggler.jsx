568ec02bfcca525976aab7c295dfb21768f4c02c
moliver226 <marcusoliver226@gmail.com>
2022-11-12 16:00:22 -0800

    Cleaning.

--- src/components/Layout/Header/ThemeToggler.jsx
+++ src/components/Layout/Header/ThemeToggler.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMagnifyingGlass, faMoon, faSun } from '@fortawesome/free-solid-svg-icons'
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons'
 
 export default function ThemeToggler({setTheme}) {
     return (
